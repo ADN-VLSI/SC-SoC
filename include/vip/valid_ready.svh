b07f7252eeccf5eada7a679b5880a644769194fd
Foez Ahmed <foez.official@gmail.com>
2026-03-08 12:44:38 +0600

    Refactor AXI4-Lite interface to utilize VALID_READY_METHODS for improved signal handling; replace AXIL_METHODS with new definitions for aw, w, b, ar, and r channels.

--- include/vip/valid_ready.svh
+++ include/vip/valid_ready.svh
@@ -1,23 +1,54 @@
 `ifndef __GUARD_VIP_VALID_READY_SVH__
 `define __GUARD_VIP_VALID_READY_SVH__ 0
 
-  `define VALID_READY_METHODS(__ARST_N__, __CLK__, __SIGNAL_T__, __SIGNAL__, __VALID__, __READY__) \
+  `define VALID_READY_METHODS(__NAME__, __ARST_N__, __CLK__, __SIGNAL_T__, __SIGNAL__, __VALID__, __READY__, __EDGE__) \
+                                                                                                                       \
+    semaphore send_``__NAME__``_sem = new(1);                                                                          \
+    task automatic send_``__NAME__``(input ``__SIGNAL_T__`` bus);                                                      \
+      send_``__NAME__``_sem.get(1);                                                                                    \
+      wait (``__EDGE__`` || !``__ARST_N__``);                                                                          \
+      ``__SIGNAL__`` <= bus;                                                                                           \
+      ``__VALID__`` <= ``__ARST_N__``;                                                                                 \
+      if (``__ARST_N__``) do @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                         \
+      while (!``__READY__`` && ``__ARST_N__``);                                                                        \
+      ``__VALID__`` <= 1'b0;                                                                                           \
+      send_``__NAME__``_sem.put(1);                                                                                    \
+    endtask                                                                                                            \
+                                                                                                                       \
+    semaphore recv_``__NAME__``_sem = new(1);                                                                          \
+    task automatic recv_``__NAME__``(output ``__SIGNAL_T__`` bus);                                                     \
+      recv_``__NAME__``_sem.get(1);                                                                                    \
+      wait (``__EDGE__`` || !``__ARST_N__``);                                                                          \
+      ``__READY__`` <= ``__ARST_N__``;                                                                                 \
+      if (``__ARST_N__``) do @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                         \
+      while (!``__VALID__`` && ``__ARST_N__``);                                                                        \
+      bus = ``__SIGNAL__``;                                                                                            \
+      ``__READY__`` <= 1'b0;                                                                                           \
+      recv_``__NAME__``_sem.put(1);                                                                                    \
+    endtask                                                                                                            \
+                                                                                                                       \
+    task automatic look_``__NAME__``(output ``__SIGNAL_T__`` bus);                                                     \
+      if (``__ARST_N__``) do @(posedge ``__CLK__`` or negedge ``__ARST_N__``);                                         \
+      while (!(``__VALID__`` & ``__READY__``) && ``__ARST_N__``);                                                      \
+      bus = ``__SIGNAL__``;                                                                                            \
+    endtask                                                                                                            \
 
 
-  `define VALID_READY_PROPERTY_CHECK(__ARST_N__, __CLK__, __SIGNAL__, __VALID__, __READY__)        \
-    assert property (@(posedge ``__CLK__``)                                                        \
-      disable iff (!``__ARST_N__``)                                                                \
-      (``__VALID__`` & !``__READY__``)                                                             \
-      |=> $stable(``__SIGNAL__``))                                                                 \
-    else                                                                                           \
-      $error(`"A valid ``__SIGNAL__`` changed while ``__READY__`` was deasserted`");               \
-                                                                                                   \
-    assert property (@(posedge ``__CLK__``)                                                        \
-      disable iff (!``__ARST_N__``)                                                                \
-      ($past(``__VALID__``) && !``__VALID__``)                                                     \
-      |=> $past(``__READY__``, 2))                                                                 \
-    else                                                                                           \
-      $error(`"The ``__VALID__`` deasserted without ``__READY__`` `");                             \
+  `define VALID_READY_PROPERTY_CHECK(__ARST_N__, __CLK__, __SIGNAL__, __VALID__, __READY__)                            \
+                                                                                                                       \
+    assert property (@(posedge ``__CLK__``)                                                                            \
+      disable iff (!``__ARST_N__``)                                                                                    \
+      (``__VALID__`` & !``__READY__``)                                                                                 \
+      |=> $stable(``__SIGNAL__``))                                                                                     \
+    else                                                                                                               \
+      $error(`"A valid ``__SIGNAL__`` changed while ``__READY__`` was deasserted`");                                   \
+                                                                                                                       \
+    assert property (@(posedge ``__CLK__``)                                                                            \
+      disable iff (!``__ARST_N__``)                                                                                    \
+      ($past(``__VALID__``) && !``__VALID__``)                                                                         \
+      |=> $past(``__READY__``, 2))                                                                                     \
+    else                                                                                                               \
+      $error(`"The ``__VALID__`` deasserted without ``__READY__`` `");                                                 \
 
 
 
--- interface/axi4l_if.sv
+++ interface/axi4l_if.sv
@@ -56,43 +56,11 @@ interface axi4l_if #(
     axi4l_rsp <= '0;
   endtask
 
-  `define AXIL_METHODS(__CHAN__, __TX__, __RX__)                                                   \
-    semaphore send_``__CHAN__``_sem = new(1);                                                      \
-    task automatic send_``__CHAN__``(input axi4l_``__CHAN__``_chan_t ``__CHAN__``);                \
-      send_``__CHAN__``_sem.get(1);                                                                \
-      wait (is_edge_aligned);                                                                      \
-      axi4l_``__TX__``.``__CHAN__``       <= ``__CHAN__``;                                         \
-      axi4l_``__TX__``.``__CHAN__``_valid <= 1'b1;                                                 \
-      do @(posedge clk_i);                                                                         \
-      while (!axi4l_``__RX__``.``__CHAN__``_ready);                                                \
-      axi4l_``__TX__``.``__CHAN__``_valid <= 1'b0;                                                 \
-      send_``__CHAN__``_sem.put(1);                                                                \
-    endtask                                                                                        \
-                                                                                                   \
-    semaphore recv_``__CHAN__``_sem = new(1);                                                      \
-    task automatic recv_``__CHAN__``(output axi4l_``__CHAN__``_chan_t ``__CHAN__``);               \
-      recv_``__CHAN__``_sem.get(1);                                                                \
-      wait (is_edge_aligned);                                                                      \
-      axi4l_``__RX__``.``__CHAN__``_ready <= 1'b1;                                                 \
-      do @(posedge clk_i);                                                                         \
-      while (!axi4l_``__TX__``.``__CHAN__``_valid);                                                \
-      ``__CHAN__``       = axi4l_``__TX__``.``__CHAN__``;                                          \
-      axi4l_``__RX__``.``__CHAN__``_ready <= 1'b0;                                                 \
-      recv_``__CHAN__``_sem.put(1);                                                                \
-    endtask                                                                                        \
-                                                                                                   \
-    task automatic look_``__CHAN__``(output axi4l_``__CHAN__``_chan_t ``__CHAN__``);               \
-      do @(posedge clk_i);                                                                         \
-      while (!(axi4l_``__TX__``.``__CHAN__``_valid & axi4l_``__RX__``.``__CHAN__``_ready));        \
-      ``__CHAN__``       = axi4l_``__TX__``.``__CHAN__``;                                          \
-    endtask                                                                                        \
-
-
-  `AXIL_METHODS(aw, req, rsp)
-  `AXIL_METHODS(w, req, rsp)
-  `AXIL_METHODS(b, rsp, req)
-  `AXIL_METHODS(ar, req, rsp)
-  `AXIL_METHODS(r, rsp, req)
+  `VALID_READY_METHODS(aw, arst_ni, clk_i, axi4l_aw_chan_t, axi4l_req.aw, axi4l_req.aw_valid, axi4l_rsp.aw_ready, is_edge_aligned)
+  `VALID_READY_METHODS(w,  arst_ni, clk_i, axi4l_w_chan_t,  axi4l_req.w,  axi4l_req.w_valid,  axi4l_rsp.w_ready,  is_edge_aligned)
+  `VALID_READY_METHODS(b,  arst_ni, clk_i, axi4l_b_chan_t,  axi4l_rsp.b,  axi4l_rsp.b_valid,  axi4l_req.b_ready,  is_edge_aligned)
+  `VALID_READY_METHODS(ar, arst_ni, clk_i, axi4l_ar_chan_t, axi4l_req.ar, axi4l_req.ar_valid, axi4l_rsp.ar_ready, is_edge_aligned)
+  `VALID_READY_METHODS(r,  arst_ni, clk_i, axi4l_r_chan_t,  axi4l_rsp.r,  axi4l_rsp.r_valid,  axi4l_req.r_ready,  is_edge_aligned)
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // ASSERTIONS
