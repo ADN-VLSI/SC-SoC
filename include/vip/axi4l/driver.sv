2a3279c500330cd9803328d6d56c570803b50503
Foez Ahmed <foez.official@gmail.com>
2026-03-10 11:31:00 +0600

    modified:   include/vip/axi4l/driver.sv

--- include/vip/axi4l/driver.sv
+++ include/vip/axi4l/driver.sv
@@ -23,6 +23,12 @@ class axi4l_driver #(
 
   mailbox #(axi4l_seq_item) mbx = new(1);
 
+  mailbox #(r_aw_chan_t) aw_mbx = new();
+  mailbox #(r_w_chan_t) w_mbx = new();
+  mailbox #(r_b_chan_t) b_mbx = new();
+  mailbox #(r_ar_chan_t) ar_mbx = new();
+  mailbox #(r_r_chan_t) r_mbx = new();
+
   virtual axi4l_if #(
       .req_t(req_t),
       .rsp_t(rsp_t)
@@ -42,15 +48,70 @@ class axi4l_driver #(
 
   task automatic run();
     axi4l_seq_item item;
-    fork
-      if (IS_MASTER) begin
+    if (IS_MASTER) begin
+      fork
+
+        // Define Transfers
         forever begin
-          mbx.peek(item);
-          init_tx(item);
           mbx.get(item);
+          if (item.is_write) begin
+            r_aw_chan_t aw;
+            r_w_chan_t  w;
+            r_b_chan_t  b;
+            aw.addr = item.addr;
+            aw.prot = 0;
+            w.data  = item.data;
+            w.strb  = item.strb;
+            aw_mbx.put(aw);
+            w_mbx.put(w);
+            b_mbx.put(b);
+          end else begin
+            r_ar_chan_t ar;
+            r_r_chan_t  r;
+            ar.addr = item.addr;
+            ar.prot = 0;
+            ar_mbx.put(ar);
+            r_mbx.put(r);
+          end
         end
-      end
-    join_none
+
+        // Handle AW
+        forever begin
+          r_aw_chan_t aw;
+          aw_mbx.get(aw);
+          vif.send_aw(aw);
+        end
+
+        // Handle W
+        forever begin
+          r_w_chan_t w;
+          w_mbx.get(w);
+          vif.send_w(w);
+        end
+
+        // Handle B
+        forever begin
+          r_b_chan_t b;
+          b_mbx.get(b);
+          vif.recv_b(b);
+        end
+
+        // Handle AR
+        forever begin
+          r_ar_chan_t ar;
+          ar_mbx.get(ar);
+          vif.send_ar(ar);
+        end
+
+        // Handle R
+        forever begin
+          r_r_chan_t r;
+          r_mbx.get(r);
+          vif.recv_r(r);
+        end
+
+      join_none
+    end
   endtask
 
   task automatic reset();
@@ -58,32 +119,6 @@ class axi4l_driver #(
       vif.req_reset();
     end else begin
       vif.rsp_reset();
-    end
-  endtask
-
-  task automatic init_tx(axi4l_seq_item item);
-    if (item.is_write) begin
-      r_aw_chan_t aw;
-      r_w_chan_t  w;
-      r_b_chan_t  b;
-      aw.addr = item.addr;
-      aw.prot = 0;
-      w.data  = item.data;
-      w.strb  = item.strb;
-      fork
-        vif.send_aw(aw);
-        vif.send_w(w);
-        vif.recv_b(b);
-      join
-    end else begin
-      r_ar_chan_t ar;
-      r_r_chan_t  r;
-      ar.addr = item.addr;
-      ar.prot = 0;
-      fork
-        vif.send_ar(ar);
-        vif.recv_r(r);
-      join
     end
   endtask
 
