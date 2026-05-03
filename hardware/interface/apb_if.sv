1dba868441f7d17b8c207042c9ab1d691bd6350a
Motasim Faiyaz <motasimfaiyaz@gmail.com>
2026-05-03 15:38:32 +0600

    FINAL CHECK 	modified:   hardware/interface/apb_if.sv

--- hardware/interface/apb_if.sv
+++ hardware/interface/apb_if.sv
@@ -1,88 +1,80 @@
 `include "package/sc_soc_pkg.sv"
-import sc_soc_pkg::*;
-
 
 interface apb_if #(
-    parameter type ADDR_WIDTH = sc_soc_pkg::ADDR_WIDTH,
-    parameter type DATA_WIDTH = sc_soc_pkg::DATA_WIDTH
+    parameter  ADDR_WIDTH = sc_soc_pkg::ADDR_WIDTH,
+    parameter  DATA_WIDTH = sc_soc_pkg::DATA_WIDTH
 
-);
+)
 (
 
     input logic arst_ni,
     input logic clk_i
-    /*input [ADDR_WIDTH-1:0] addr_i;
-    input [DATA_WIDTH-1:0] wdata_i;
-    input [(DATA_WIDTH/8)-1:0] wstrb_i= sc_soc_pkg::DATA_WIDTH/8{1'b1};
-    output logic [DATA_WIDTH-1:0] rdata_o;
-    output logic pready_o; */
+
 );
+    import sc_soc_pkg::*;
 
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  // TYPEDEFS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  //`APB_TYPEDEF_ALL(apb, logic[ADDR_WIDTH-1:0], logic[DATA_WIDTH-1:0], logic[DATA_WIDTH/8-1:0])
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  // SIGNALS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+   // SIGNALS
+  /////////////////////////////////////////////////////////////////////////////////////////////////
 
   apb_req_t req;
   apb_resp_t resp;
 
 
-/*logic psel_i;
-  logic penable_i;
-  logic pwrite_i;
-  logic pready_o;
-  logic [DATA_WIDTH-1:0] prdata_o; */
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  // METHODS
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+   // METHODS
+  /////////////////////////////////////////////////////////////////////////////////////////////////
 
 task automatic apb_read(
-    input logic [ADDR_WIDTH-1:0] req.paddr,
-    output logic [DATA_WIDTH-1:0] req.rdata,
-    output logic resp.pready
+    input logic [ADDR_WIDTH-1:0] paddr,
+    output logic [DATA_WIDTH-1:0] pdata
+    //output logic ready
     );
+    req.paddr <= paddr; 
+    
 
-    req.pwrite <= 1'b0; // First, set pwrite_i to 0 for a read operation
-    req.psel <= 1'b1; // Then assert psel_i to select the slave
-    @(posedge clk_i); // wait for one clock cycle
-    req.penable <= 1'b1; //Then assert penable_i
+    req.pwrite <= 1'b0;                                  // First, set pwrite_i to 0 for a read operation
+    req.psel <= 1'b1;                                   // Then assert psel_i to select the slave
+    req.penable <= 1'b0;                                 // Ensure penable_i is low at the start of the read operation
+    @(posedge clk_i);                                 // wait for one clock cycle
+    req.penable <= 1'b1;                              //Then assert penable_i
 
     do begin
-        @(posedge clk_i); // wait for the next clock cycle
-    end while (resp.pready == 1'b0); // wait until pready_o is asserted
+        @(posedge clk_i);                         // wait for the next clock cycle
+    end while (resp.pready == 1'b0);             // wait until pready_o is asserted
     
+    pdata = resp.prdata;                       // Connect the output data to the response structure 
+    req.pwrite <= 1'b0;                        // Deassert pwrite_i after the read operation is complete
+    req.psel <= 1'b0;                         // Deassert psel_i to deselect the slave
+    req.penable <= 1'b0;                     // Deassert penable_i to complete the read operation
+
+
 endtask
 
 task automatic apb_write(
-    input logic [ADDR_WIDTH-1:0] req.paddr,
-    input logic [DATA_WIDTH-1:0] req.pwdata,
-    input logic [DATA_WIDTH/8-1:0] req.pwstrb,
-    output logic resp.pready
+    input logic [ADDR_WIDTH-1:0] paddr,
+    input logic [DATA_WIDTH-1:0] pdata,
+    input logic [DATA_WIDTH/8-1:0] pwstrb
+    //output logic ready
     );
-    req.pwrite <= 1'b1;
+
+    req.paddr <= paddr;
+    req.pwdata <= pdata;                                  // Connect the input data to the request structure
+    req.pstrb <= pwstrb;                                 // Connect the write strobe to the request structure
+   
+    req.pwrite <= 1'b1;                                // First, set pwrite_i to 1 for a write operation
     req.psel <= 1'b1;
-    @(posedge clk_i); // wait for one clock cycle
-    req.penable <= 1'b1; //Then assert penable_i
+    req.penable <= 1'b0;                                 // Ensure penable_i is low at the start of the write operation
+    @(posedge clk_i);                               // wait for one clock cycle
+    req.penable <= 1'b1;                            //Then assert penable_i
     do begin
-        @(posedge clk_i); // wait for the next clock cycle
-    end while (resp.pready == 1'b0); // wait until pready_o is asserted
+        @(posedge clk_i);                        // wait for the next clock cycle
+    end while (resp.pready == 1'b0);            // wait until pready_o is asserted
     
+    req.pwrite <= 1'b0;                        // Deassert pwrite_i after the write operation is complete
+    req.psel <= 1'b0;                         // Deassert psel_i to deselect the slave
+    req.penable <= 1'b0;                     // Deassert penable_i to complete the write operation
     
 endtask
-
-
-
-
-
-
-
-
 
 endinterface
