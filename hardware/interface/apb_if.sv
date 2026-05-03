a20e7e33b90386ca7eee5197a185dd4d78b5cdb4
Motasim Faiyaz <motasimfaiyaz@gmail.com>
2026-05-03 13:12:39 +0600

    APB_IF draft 	modified:   hardware/interface/apb_if.sv

--- hardware/interface/apb_if.sv
+++ hardware/interface/apb_if.sv
@@ -1,63 +1,78 @@
 `include "package/sc_soc_pkg.sv"
-
 import sc_soc_pkg::*;
-interface apb_if #(
-    parameter int ADDR_WIDTH = sc_soc_pkg::ADDR_WIDTH,
-    parameter int DATA_WIDTH = sc_soc_pkg::DATA_WIDTH
-) (
 
-    input logic arst_ni;
-    input logic clk_i;
-    input [ADDR_WIDTH-1:0] addr_i;
+
+interface apb_if #(
+    parameter type ADDR_WIDTH = sc_soc_pkg::ADDR_WIDTH,
+    parameter type DATA_WIDTH = sc_soc_pkg::DATA_WIDTH
+
+);
+(
+
+    input logic arst_ni,
+    input logic clk_i
+    /*input [ADDR_WIDTH-1:0] addr_i;
     input [DATA_WIDTH-1:0] wdata_i;
     input [(DATA_WIDTH/8)-1:0] wstrb_i= sc_soc_pkg::DATA_WIDTH/8{1'b1};
     output logic [DATA_WIDTH-1:0] rdata_o;
-    output logic pready_o;
+    output logic pready_o; */
 );
+
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // TYPEDEFS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  `APB_TYPEDEF_ALL(apb, logic[ADDR_WIDTH-1:0], logic[DATA_WIDTH-1:0], logic[DATA_WIDTH/8-1:0])
+  //`APB_TYPEDEF_ALL(apb, logic[ADDR_WIDTH-1:0], logic[DATA_WIDTH-1:0], logic[DATA_WIDTH/8-1:0])
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // SIGNALS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-  apb_req_t apb_req;
-  apb_resp_t apb_resp;
+  apb_req_t req;
+  apb_resp_t resp;
 
-  logic psel_i;
+
+/*logic psel_i;
   logic penable_i;
   logic pwrite_i;
   logic pready_o;
-  logic [DATA_WIDTH-1:0] prdata_o;
+  logic [DATA_WIDTH-1:0] prdata_o; */
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
   // METHODS
   //////////////////////////////////////////////////////////////////////////////////////////////////
 
-task automatic apb_read(addr_i, rdata_o, pready_o);
-    pwrite_i <= '0;
-    psel_i   <= '1;
-    repeat (1) @(posedge clk_i); // wait for one clock cycle
-    penable_i <= '1; //Then assert penable_i
+task automatic apb_read(
+    input logic [ADDR_WIDTH-1:0] req.paddr,
+    output logic [DATA_WIDTH-1:0] req.rdata,
+    output logic resp.pready
+    );
+
+    req.pwrite <= 1'b0; // First, set pwrite_i to 0 for a read operation
+    req.psel <= 1'b1; // Then assert psel_i to select the slave
+    @(posedge clk_i); // wait for one clock cycle
+    req.penable <= 1'b1; //Then assert penable_i
 
     do begin
         @(posedge clk_i); // wait for the next clock cycle
-    end while (pready_o == '0); // wait until pready_o is asserted
+    end while (resp.pready == 1'b0); // wait until pready_o is asserted
     
 endtask
 
-task automatic apb_write(addr_i, wdata_i, wstrb_i,pready_o);
-    pwrite_i <= '1;
-    psel_i <= '1;
-    repeat (1) @(posedge clk_i); // wait for one clock cycle
-    penable_i <= '1; //Then assert penable_i
+task automatic apb_write(
+    input logic [ADDR_WIDTH-1:0] req.paddr,
+    input logic [DATA_WIDTH-1:0] req.pwdata,
+    input logic [DATA_WIDTH/8-1:0] req.pwstrb,
+    output logic resp.pready
+    );
+    req.pwrite <= 1'b1;
+    req.psel <= 1'b1;
+    @(posedge clk_i); // wait for one clock cycle
+    req.penable <= 1'b1; //Then assert penable_i
     do begin
         @(posedge clk_i); // wait for the next clock cycle
-    end while (pready_o == '0); // wait until pready_o is asserted
+    end while (resp.pready == 1'b0); // wait until pready_o is asserted
     
     
 endtask
